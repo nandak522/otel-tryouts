787528d564cf6cb18907fda3291ac3a77973d877
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2020-10-03 00:11:40 +0530

    Induce sleep

--- backend-notifications/pkg/notifications/main.go
+++ backend-notifications/pkg/notifications/main.go
@@ -1,7 +1,10 @@
 package notifications
 
+import "time"
+
 // GetNotifications ...
 func GetNotifications() []string {
+	time.Sleep(250 * time.Millisecond) // Analogous to a db call
 	notifications := []string{
 		"Read this 1",
 		"Read this 2",
--- backend-tweets/pkg/tweets/main.go
+++ backend-tweets/pkg/tweets/main.go
@@ -1,7 +1,10 @@
 package tweets
 
+import "time"
+
 // GetTweets ...
 func GetTweets() []string {
+	time.Sleep(250 * time.Millisecond) // Analogous to a db call
 	tweets := []string{
 		"Tweet 1",
 		"Tweet 2",

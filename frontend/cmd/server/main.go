2f2a666ac95fa8003441de93769f30f2e24b6bbc
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2020-10-03 11:31:03 +0530

    Add newrelic instrumentation for frontend

--- frontend/cmd/server/main.go
+++ frontend/cmd/server/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	flag "github.com/spf13/pflag"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 // StartTime gives the start time of server
 // var StartTime = time.Now()
-
 
 // func uptime() string {
 // 	elapsedTime := time.Since(StartTime)
@@ -39,6 +41,24 @@ func main() {
 
 	log.SetLevel(log.DebugLevel)
 	log.Info("Running Frontend Service on ", port, "...")
-	http.HandleFunc("/", homepage)
+	newrelicLicenseKey, isEnvVarSet := os.LookupEnv("NEWRELIC_LICENSE_KEY")
+	if isEnvVarSet {
+		var newrelicAPM string
+		newrelicAPM, isEnvVarSet := os.LookupEnv("NEWRELIC_APM")
+		if !isEnvVarSet {
+			newrelicAPM = "twitter-frontend"
+		}
+		app, _ := newrelic.NewApplication(
+			newrelic.ConfigAppName(newrelicAPM),
+			newrelic.ConfigLicense(newrelicLicenseKey),
+			// add debug level logging to stdout
+			newrelic.ConfigDebugLogger(os.Stdout),
+		)
+		log.Info("Newrelic Monitoring is enabled. Posting to ", newrelicAPM, " APM")
+		http.HandleFunc(newrelic.WrapHandleFunc(app, "/", homepage))
+	} else {
+		log.Info("NEWRELIC_LICENSE_KEY env variable not found. Instrumentation is off. Moving on...")
+		http.HandleFunc("/", homepage)
+	}
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }

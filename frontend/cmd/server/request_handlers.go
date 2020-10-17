88d27ca8af0b4421f828eba69383db710afeab31
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2020-10-17 21:03:26 +0530

    make calls parallely

--- frontend/cmd/server/request_handlers.go
+++ frontend/cmd/server/request_handlers.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 
 	apm "github.com/newrelic/go-agent/v3/newrelic"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
@@ -17,19 +19,11 @@ func handleErrorResponse(w http.ResponseWriter, err error) {
 	fmt.Fprintf(w, err.Error())
 }
 
-func homepage(w http.ResponseWriter, r *http.Request) {
-	txn := apm.FromContext(r.Context())
-	defer txn.End()
-
-	requestContext := r.Context()
+func makeExternalCall(rootSpanContext *context.Context, url string, wg *sync.WaitGroup) string {
+	defer wg.Done()
+	req, _ := http.NewRequest("GET", url, nil)
+	otelhttptrace.Inject(*rootSpanContext, req)
 	frontEndClient := http.DefaultClient
-	var tweets string
-	var notifications string
-	tracer := global.Tracer("frontend")
-	rootSpanContext, rootSpan := tracer.Start(requestContext, "/homepage")
-	req, _ := http.NewRequest("GET", "http://localhost:8001", nil)
-	otelhttptrace.Inject(rootSpanContext, req)
-
 	tweetsResponse, err := frontEndClient.Do(req)
 	if err != nil {
 		fmt.Println("Error from Tweets Service")
@@ -40,21 +34,29 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Error in reading tweetsResponse.Body")
 	}
-	tweets = string(body)
+	return string(body)
+}
 
-	req, _ = http.NewRequest("GET", "http://localhost:8002", nil)
-	otelhttptrace.Inject(rootSpanContext, req)
-	notificationsResponse, err := frontEndClient.Do(req)
-	if err != nil {
-		fmt.Println("Error from Notifications Service")
-	}
-	defer notificationsResponse.Body.Close()
+func homepage(w http.ResponseWriter, r *http.Request) {
+	txn := apm.FromContext(r.Context())
+	defer txn.End()
 
-	body, err = ioutil.ReadAll(notificationsResponse.Body)
-	if err != nil {
-		fmt.Println("Error in reading notificationsResponse.Body")
-	}
-	notifications = string(body)
+	requestContext := r.Context()
+	tracer := global.Tracer("frontend")
+	rootSpanContext, rootSpan := tracer.Start(requestContext, "/homepage")
+
+	var tweets string
+	var notifications string
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		tweets = makeExternalCall(&rootSpanContext, "http://localhost:8001", &wg)
+	}()
+	wg.Add(1)
+	go func() {
+		notifications = makeExternalCall(&rootSpanContext, "http://localhost:8002", &wg)
+	}()
+	wg.Wait()
 
 	rootSpan.End()
 
